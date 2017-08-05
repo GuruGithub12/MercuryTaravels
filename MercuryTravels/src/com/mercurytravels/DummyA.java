36bcefa2fb1fc991667e56be626e2b567fd41d65
Guru <gurug.qa@gmail.com>
2017-08-05 13:39:59 +1000

    Converted to Maven Project and downloaded dependencies

--- MercuryTravels/src/com/mercurytravels/DummyA.java
+++ MercuryTravels/src/com/mercurytravels/DummyA.java
@@ -5,6 +5,7 @@ public class DummyA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Mercury");
+		System.out.println("Hello Testing World");
 	}
 
 }
